bc6820c33317e245d35fb01203ff085762a0c898
Tofan-Jena <47977011+Tofan-Jena@users.noreply.github.com>
2021-07-16 22:57:06 +0530

    Update iis.ps1

--- iis.ps1
+++ iis.ps1
@@ -1,3 +1,7 @@
 Install-WindowsFeature -name Web-Server -IncludeAllSubFeature -IncludeManagementTools
+Install-Module -Name 'IISAdministration'
+New-Item -ItemType Directory -Name 'MyWebsite' -Path 'C:\'
+New-Item -ItemType File -Name 'index.html' -Path 'C:\MyWebsite\'
+New-IISSite -Name 'MyWebsite' -PhysicalPath 'C:\MyWebsite\' -BindingInformation "*:8088:"
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 New-Item -Path c:\ -Name Temp -ItemType Directory
